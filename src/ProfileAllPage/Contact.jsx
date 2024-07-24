198cd9e63890231cffd0fc5bd802e0069bfef95d
arun <hgsarun@gmail.com>
2024-07-24 10:43:18 +0530

    ads

--- src/ProfileAllPage/Contact.jsx
+++ src/ProfileAllPage/Contact.jsx
@@ -101,7 +101,7 @@ export default function Contact() {
           </Grid>
         </Grid>
       </Box>
-      <Box sx={{minWidth:'250px',maxWidth:"100%",position:'fixed',top:'0',left:0}}>
+      <Box sx={{minWidth:'250px',position:'fixed',top:'0',left:0}}>
       <AdsComponent />
       <AdsComp/>
       </Box>
