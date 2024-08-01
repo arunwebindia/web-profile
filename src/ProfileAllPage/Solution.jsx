78aae7b2cf256a0e1f17aeac34548eb661eedd71
arun <hgsarun@gmail.com>
2024-08-01 12:56:26 +0530

    add more ques

--- src/ProfileAllPage/Solution.jsx
+++ src/ProfileAllPage/Solution.jsx
@@ -8,10 +8,10 @@ export default function Solution({programming}) {
     const [viewNew, setViewNew] = useState(true);
 
     return (
-        <Box sx={{ width: '100%', minHeight: '100vh', display: 'flex', backgroundColor: '#ffd1c233', alignItems: 'center', justifyContent: 'center', padding: '1.5rem 0' }} id="education">
-            <Box className="section-wrapper">
+        <Box sx={{ width: '100%', minHeight: '100vh', display: 'flex', backgroundColor: '#ffd1c233', alignItems: 'center', justifyContent: 'center', padding: '1.5rem 0' }} id="solution">
+            <Box className="section-wrapper" sx={{maxWidth:'95%S'}}>
                 <Box className="ques-box" border={".3px solid lightblue"} p={1} >
-                    <Box className="ques" sx={{ padding: '.5rem', fontSize: '1.6rem', borderLeft: '4px solid red' }}>
+                    <Box className="ques" sx={{ padding: '.5rem', fontSize: '1.6rem', borderLeft: '4px solid red',color:'green' }}>
                         <Typography>
                             {programming.ques}
                         </Typography>
@@ -19,11 +19,11 @@ export default function Solution({programming}) {
                     <Button variant="outlined" size="small" sx={{ marginTop: '.5rem', marginBottom: ".5rem" }} onClick={() => setViewAns(!vewAns)}>
                         {vewAns ? 'view' : 'hide'} answer
                     </Button>
-                    <Box className="ans" sx={{ border: ".2px solid lightgray", padding: "0 1rem", background: "#fff" }} style={{ height: `${vewAns ? '0px' : ''}`, overflow: `${vewAns ? 'hidden' : ''}` }}>
+                    <Box className="ans" sx={{ border: ".2px solid lightgray",background: "#000",color:"white",}} style={{ height: `${vewAns ? '0px' : ''}`, overflow: `${vewAns ? 'hidden' : 'auto'}` }}>
                         <pre>{programming.ansText}</pre>
                     </Box>
                     <Box className="post" sx={{ padding: '10px 0' }}>
-                        <Box sx={{}}>
+                        <Box>
                             <FormLabel sx={{ marginBottom: '.5rem' }}>Send your code</FormLabel>
                             <Textarea minRows={5} size="sm" name="message" />
                         </Box>
--- src/utility_files/utility.jsx
+++ src/utility_files/utility.jsx
@@ -88,20 +88,222 @@ export const programming =[
         ques:"Write a JavaScript function to generate a random integer.",
         ansText:`
         function randum(max,min){
-            if(max == null && min == null){
-                return 0
-            }
-            else{
-                if(max == null){
-                    max = min;
-                }
-                return min + Math.floor(Math.random()*(max - min +1))
-            }
+    if(max == null && min == null){
+        return 0
+    }
+    else{
+        if(max == null){
+            max = min;
         }
-        console.log(randum(5, 8))
+        return min + Math.floor(Math.random()*(max - min +1))
+    }
+}
+console.log(randum(5, 8))
         `,
         ansValue:''
         
+    },
+    {
+        ques:"Write a JavaScript function to format a number up to specified decimal places.",
+        ansText:`
+        function fn(num,size){
+  return num.toFixed(size)
+}
+console.log(fn(2100, 2))
+
+6. Write a JavaScript function to find the highest value in an array.
+function hst(arr){
+  let n = Number.NEGATIVE_INFINITY;
+  arr.forEach((elem)=>{
+    if(elem > n){
+      n = elem;
     }
+    
+  })
+  return n
+}
+console.log(hst([12,34,56,1]))
+
+        `,
+        ansValue:''
+        
+    },
+    {
+        ques:"Write a JavaScript function to find the lowest value in an array.",
+        ansText:`
+        function low(arr){
+    let lowValue = Number.POSITIVE_INFINITY;
+    arr.forEach((elem,ind)=>{
+        if(elem < lowValue){
+            lowValue =  elem
+        }
+    })
+    return lowValue;
+}
+console.log(low(([-12,-34,0,-56,-1])))
+        `,
+        ansValue:''
+        
+    },
+    {
+        ques:"Write a JavaScript function to find the GCD (greatest common divisor) of more than 2 integers.",
+        ansText:`
+        function fn(arr){
+  let newarr = arr.sort((a,b)=>a - b);
+  let i = newarr[0];
+  while(i>0){
+    if(newarr.every((elem)=>elem % i == 0)){
+    return i
+  }
+  i--
+  }
+  
+  
+}
+console.log(fn([3,15,9]))
+        `,
+        ansValue:''
+        
+    },{
+        ques:" Write a JavaScript function to get the least common multiple (LCM) of more than 2 integers.",
+        ansText:`
+    function fn(arr){
+  let big = Number.NEGATIVE_INFINITY;
+  arr.forEach((elem)=>{
+    if(elem > big){
+      big = elem
+    }
+  })
+  while(big < arr.reduce((elem)=>elem *=elem)){
+  if(arr.every((elem)=>big % elem == 0)){
+    return big
+  }
+  big++;
+  }
+}
+console.log(fn([100,90,80,7]))
+        `,
+        ansValue:''
+        
+    },
+    {
+        ques:" Write a JavaScript function to test if a number is a power of 2.",
+        ansText:`
+   function fn(num){
+  let numm = 2;
+  while(numm < num){
+    numm = Math.pow(numm,2);
+  }
+ return (numm === num) ? true : false;
+}
+console.log(fn(256))
+        `,
+        ansValue:''
+        
+    },
+    {
+        ques:" Write a JavaScript function to get the least common multiple (LCM) of more than 2 integers.",
+        ansText:`
+   function fn(arr){
+  let big = Number.NEGATIVE_INFINITY;
+  arr.forEach((elem)=>{
+    if(elem > big){
+      big = elem
+    }
+  })
+  while(big < arr.reduce((elem)=>elem *=elem)){
+  if(arr.every((elem)=>big % elem == 0)){
+    return big
+  }
+  big++;
+  }
+}
+console.log(fn([100,90,80,7]))
+        `,
+        ansValue:''
+        
+    },
+    {
+        ques:" Write a JavaScript function to test if a number is a power of 2.",
+        ansText:`
+   function fn(num){
+  let numm = 2;
+  while(numm < num){
+    numm = Math.pow(numm,2);
+  }
+ return (numm === num) ? true : false;
+}
+        `,
+        ansValue:''
+        
+    },
+    {
+        ques:"Write a JavaScript function to round a number to decimal place.",
+        ansText:`
+  function fn(num,n){
+  return num.toFixed(n)
+}
+console.log(fn(10.49999,2))
+        `,
+        ansValue:''
+        
+    },
+    {
+        ques:"Write a JavaScript function to check whether a value is an integer or not.",
+        ansText:`
+function fn(num){
+  return num == num.toFixed(0)
+}
+console.log(fn(235.235))
+
+        `,
+        ansValue:''
+        
+    },
+    {
+        ques:"Write a JavaScript function to calculate the product of values in an array.",
+        ansText:`
+function fn(arr){
+  return arr.reduce((total,elem)=> total *= elem)
+}
+console.log(fn([100,-200,3]))
+        `,
+        ansValue:''
+        
+    },
+    {
+        ques:"Create a Pythagorean function in JavaScript.",
+        ansText:`
+function fn(a,b){
+  return Math.pow(Math.pow(a,2) + Math.pow(b,2),0.5)
+}
+console.log(fn(2, 4))
+        `,
+        ansValue:''
+        
+    },
+    {
+        ques:"Write a JavaScript program to evaluate binomial coefficients.",
+        ansText:`
+function fn(a,b){
+  let d = a-b;
+  let val = 1;
+  while(a > 1 || b > 1 || d > 1){
+    val *= a/(b*d)
+    a--
+    if(b>1){
+      b--;
+    }
+    if(d>1){
+      d--
+    }
+  }
+  return Math.round(val)
+}
+console.log(fn(10,2))
+        `,
+        ansValue:''
+        
+    },
 ]
 
